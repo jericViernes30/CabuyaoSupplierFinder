4075e6ddf10e36edd9c1bc524642f71fb4d379c2
Jeric James Viernes <jericviernes06@gmail.com>
2025-01-29 21:20:44 +0800

    added to_pay data

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,20 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\Reserve;
+use App\Models\Retailer;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function addToCart(Request $request){
         $order_id = 'ORD-'.$request->input('user_id').'-'.rand(1000, 9999);
+        $dealerID = Retailer::where('business_name', $request->input('dealer_id'))->first()->id;
+        // dd($dealerID);
 
         $order = [
             'order_id' => $order_id,
             'retailer' => $request->input('user_id'),
             'rice_id' => $request->input('rice_id'),
-            'dealer_id' => $request->input('dealer_id'),
+            'dealer_id' => $dealerID,
             'order_type' => $request->input('measurement'),
             'count' => $request->input('sacks_count'),
+            'to_pay' => 0,
             'status' => 'Pending'
         ];
         // dd($order);
@@ -36,12 +40,15 @@ class OrderController extends Controller
     {
         // Get the user ID from the session
         $userID = session('profile')->id;
+        $toPay = $request->input('to_pay');
         $deliveryDate = $request->input('delivery_date');
 
         // Extract rice_ids from the orders array in the request
         $orderID = $request->input('order_id');
 
-        Order::where('order_id', $orderID)->update(['status' => 'Order Placed']);
+        Order::where('retailer', $userID)
+            ->where('status', 'Pending')
+            ->update(['status' => 'Order Placed', 'to_pay' => $toPay]);
 
         $deliveryData = [
             'order_id' => $orderID,
